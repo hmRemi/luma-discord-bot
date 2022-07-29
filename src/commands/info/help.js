5dd7dec216e70be135b85d31edfb5c58279746a3
ziue <remig06@hotmail.com>
2022-07-29 12:01:49 +0200

    helpcmdfix

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -24,8 +24,10 @@ module.exports = {
 		.addField(`**Music System**`, `\`\`\`play, volume, filter, repeat, skip, queue, pause, resume, previous, nowplaying, stop, search\`\`\``)
 		.addField(`**Moderation System**`, `\`\`\`ban, kick, purge, slowmode, lock, unlock, timeout\`\`\``)
 		.addField(`**Giveaway System**`, `\`\`\`start, end, pause, unpause, reroll, delete\`\`\``)
+    	.addField(`**Hypixel System**`, `\`\`\`hypixel (profile, skywars, bedwars) (user)\`\`\``)
 		.addField(`**Verification System**`, `\`\`\`panel, setrole, settings\`\`\``)
 		.addField(`**Information**`, `\`\`\`info, status, ping, help\`\`\``)
+    	.addField(`**Economy System**`, `\`\`\`balance\`\`\``)
 		.addField(`**Event System**`, `\`\`\`gtn\`\`\``)
 
 
